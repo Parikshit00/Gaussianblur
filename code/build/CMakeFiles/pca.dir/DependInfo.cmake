6cb42b3a3483c6a8edf179d47f80dfc2652cdaf2
parikshit.khanal <parikshit.khanal@ekbana.info>
2020-11-08 15:14:01 +0545

    aa

---
+++ code/build/CMakeFiles/pca.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/main.cpp" "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/build/CMakeFiles/pca.dir/main.cpp.o"
+  "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/src/covariance.cpp" "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/build/CMakeFiles/pca.dir/src/covariance.cpp.o"
+  "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/src/eigenQR.cpp" "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/build/CMakeFiles/pca.dir/src/eigenQR.cpp.o"
+  "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/src/mnistreader.cpp" "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/build/CMakeFiles/pca.dir/src/mnistreader.cpp.o"
+  "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/src/pca.cpp" "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/build/CMakeFiles/pca.dir/src/pca.cpp.o"
+  "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/src/subtractmean.cpp" "/home/mr-one/Desktop/PCA-From-Scratch-MNIST/code/build/CMakeFiles/pca.dir/src/subtractmean.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/build/CMakeFiles/pca.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pca.dir/main.cpp.o"
+  "CMakeFiles/pca.dir/src/covariance.cpp.o"
+  "CMakeFiles/pca.dir/src/eigenQR.cpp.o"
+  "CMakeFiles/pca.dir/src/mnistreader.cpp.o"
+  "CMakeFiles/pca.dir/src/pca.cpp.o"
+  "CMakeFiles/pca.dir/src/subtractmean.cpp.o"
+  "pca"
+  "pca.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
